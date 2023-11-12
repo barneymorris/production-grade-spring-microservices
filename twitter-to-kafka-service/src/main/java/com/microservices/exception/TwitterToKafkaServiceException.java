5ed7be9194ee1ae3d096f73b8b63216bb025a589
bsyrpin <bsyrpin@moysklad.ru>
2023-11-12 21:16:32 +0500

    Mock Twitter to Kafka Stream Runner Implementation

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/exception/TwitterToKafkaServiceException.java
@@ -0,0 +1,16 @@
+package com.microservices.exception;
+
+public class TwitterToKafkaServiceException extends RuntimeException {
+
+    public TwitterToKafkaServiceException() {
+        super();
+    }
+
+    public TwitterToKafkaServiceException(String message) {
+        super(message);
+    }
+
+    public TwitterToKafkaServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
